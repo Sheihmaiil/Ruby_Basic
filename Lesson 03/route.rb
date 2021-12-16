fb255b99f3c5e34171d088aa021aad8279d8a720
Sheih_maiil <Sheih_maiil@mail.ru>
2021-12-16 19:16:29 +0300

    Lesson 03 satation.rb

--- Lesson 03/route.rb
+++ Lesson 03/route.rb
@@ -1,26 +1,26 @@
 class Route
+  attr_accessor :route
 
-  #  def initialize(first_station = String.new(""), last_station = String.new(""))
   def initialize(first_station, last_station)
     @route =[first_station, last_station]
   end
 
   def add_station(station_name)
-    @route.insert((@route.size - 1), station_name)
+    route.insert((route.size - 1), station_name)
   end
 
   def del_station(station_name)
     
-    if station_name == @route[0] || station_name == @route[-1]
-      puts "Нельзя удалять начальную и конечную станции!!!"
+    if station_name == route.first || station_name == route.last
+      puts "Нельзя удалять начальную или конечную станции!!!"
     else
-      @route.delete(station_name)
+      route.delete(station_name)
     end
 
   end
 
   def stations_list
-    @route.each {|station| puts "#{station}"}
+    route.each {|station| puts "#{station}"}
   end
 
 end
