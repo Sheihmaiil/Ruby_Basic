da69d6fc199b7c20c7dc245b4a747eef4502fbab
Sheih_maiil <Sheih_maiil@mail.ru>
2021-12-21 00:54:37 +0300

    Lesson 03 final final

--- Lesson 03/main.rb
+++ Lesson 03/main.rb
@@ -12,24 +12,17 @@ class Station
   end
 
   def del_train(train)
-    index = @trains_list.index(train)
-    if index
-      @trains_list.slice!(index)
-    else
-      puts "На станции нет поезда #{train.train_name}"
-    end
+    @trains_list.delete(train)
   end
 
   def trains_list_by_type(train_type)
     @trains_list.select do |train|
-      train.train_type == train_type
+      train.type == train_type
     end
   end
 
   def trains_qty_by_type(train_type)
-    @trains_list.select do |train|
-      train.train_type == train_type
-    end.count
+    trains_list_by_type(train_type).count
   end
 end
 
@@ -37,47 +30,31 @@ class Route
   attr_accessor :stations_list
 
   def initialize(first_station, last_station)
-    @stations_list =[first_station, last_station]
+    @stations_list = [first_station, last_station]
   end
 
   def add_station(station)
-    @stations_list.insert((@stations_list.size - 1), station)
+    @stations_list.insert(-2, station)
   end
 
   def del_station(station)
-    if station == @stations_list.first || station == @stations_list.last
-      puts "Нельзя удалять начальную или конечную станции!!!"
-    else
-      if @stations_list.index(station).nil?
-        puts "В маршруте нет станции #{station.name}"
-      else
-        @stations_list.delete(station)
-      end
-    end
-  end
-
-  def stations_list_to_scr #Вывод списка станций на консоль
-    @stations_list.each { |station| puts "#{station.name}"}
+    @stations_list.delete(station) unless [@stations_list.first, @stations_list.last].include?(station)
   end
 end
 
 class Train
-  attr_accessor :train_name
-  attr_accessor :train_type
-  attr_accessor :current_station
-  attr_accessor :current_speed
-  attr_accessor :wagon_qty
+  attr_accessor :name, :type, :current_station_index, :current_speed, :wagon_qty
   attr_reader :route
 
-  def initialize(train_name, train_type, wagon_qty = 0)
-    @train_name = train_name
-    @train_type = train_type
+  def initialize(name, type, wagon_qty = 0)
+    @name = name
+    @type = type
     @wagon_qty = wagon_qty
     @current_speed = 0
   end
   
   def change_speed(speed) #изменение скорости может быть отрицательным
-    if @current_speed + speed < 0
+    if (@current_speed + speed).negative?
       puts "Нельзя уменьшитьскорость меньше нуля!"
     else
       @current_speed += speed
@@ -88,47 +65,41 @@ class Train
     @current_speed = 0
   end
 
-  def change_wagon_qty(wagon) #количество вагонов может быть отрицательным
-    if @current_speed != 0
-      puts "Операция невозможна - поезд в движении!"
-    elsif wagon.abs > 1
-      puts "Операция невозможна - вагонов больше одного!"
-    elsif @wagon_qty + wagon < 0
-      puts "Операция невозможна - вагонов в составе нет!"
-    else
-      @wagon_qty += wagon
-    end      
+  def add_wagon
+    @wagon_qty += 1 if @current_speed.zero?
+  end
+
+  def del_wagon
+    @wagon_qty -= 1 if @current_speed.zero? && wagon_qty.positive?
   end
 
   def add_route(route)
     @route = route
-    @current_station = 0
+    @current_station_index = 0
   end
 
-  def move_train(qty) #количество станций может быть отрицательным
-    if @route == nil
-      puts "Перемещение невозможно - не задан маршрут!"
-    elsif qty.abs > 1
-      puts "Перемешение невозможно более чем на одну станцию!"
-    elsif @current_station + qty < 0
-      puts "Перемещение невозможно - достигнута начальная станция!"
-    elsif @current_station + qty + 1 > @route.stations_list.size
-      puts "Перемещение невозможно - достигнута конечная станция!"
-    else
-      @current_station += qty
-    end
+  def move_train_forward
+    @current_station_index += 1 if @route.nil? && (@current_station_index + 1 != @route.stations_list.size)
   end
 
-  def return_stations
-    stations_names = []
-    for i in -1..1
-      if @current_station + i < 0 || (@current_station + i + 1) > @route.stations_list.size
-        stations_names << nil
-      else
-        stations_names << @route.stations_list[@current_station + i]
-      end
-    end
-
-    return stations_names
+  def move_train_backward
+    @current_station_index -= 1 if @route.nil? && (@current_station_index != 0)
   end
+
+  def current_station
+    @route.stations_list[@current_station_index]
+  end
+
+  def previous_station
+    return if @current_station_index.zero?
+
+    @route.stations_list[@current_station_index - 1]
+  end
+
+  def next_station
+    return if @current_station_index + 1 == @route.stations_list.size
+
+    @route.stations_list[@current_station_index + 1]
+  end
+
 end
